8a502f7726b26dca689cdafaa255bb51c1920831
dimarchuk <dimka82132@gmail.com>
2018-04-01 22:17:39 +0300

    L4

--- homeTask/L4/15.js
+++ homeTask/L4/15.js
@@ -1,11 +1,19 @@
-var str = "#ff00FF";
+var str = "#ff0044";
 
+/**
+ *
+ * @param str
+ * @returns {Array}
+ */
 function hexToRGB(str) {
-    var newStr = str.replace('#','');
-   for (var i = 0; i < newStr.length/2; i++) {
-      console.log(newStr.substring(i,i++));
-   }
-
+    var rez = [];
+    var count = str.length;
+    if (count <= 7) {
+        for (var i = 1; i < count; i += 2) {
+            rez.push(parseInt(str[i] + str[i + 1], 16));
+        }
+        return rez;
+    } else return "Невірні дані";
 };
 
 hexToRGB(str);
