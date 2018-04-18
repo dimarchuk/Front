35738d794b1c6cbd78bf051131f75c6f45193067
u_programmer2 programmer2 <u_programmer2@programmer2.METEO>
2018-04-18 17:04:44 +0300

    qwe

---
+++ homeTask/localStorage/script.js
@@ -0,0 +1,9 @@
+var form = document.forms[0];
+
+// for (var i = 0; i < form.elements.length; i++)
+    // form.elements[i].name
+
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+});
